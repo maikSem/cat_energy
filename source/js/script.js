b87e51c073a44b9d6f8af68bc025797827cee1c5
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-09-07 22:41:27 +0300

    Подключил js + проверил написание стилей. Сделал стилизаию активного состояния элементов навигации по сайту. На проверку

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,21 @@
 var menu = document.querySelector(".main-nav");
-var menuOpen = document.querySelector(".main-nav--opened");
-var menuClose = document.querySelector(".main-nav--closed");
-var menuButton = document.querySelector(".main-nav__toggle")
+var menuButton = document.querySelector(".main-nav__toggle");
+var mainLink = document.querySelector(".main-nav__link")
+
+if (menu.classList.contains("main-nav--no-js")) {
+  menu.classList.remove("main-nav--no-js");
+}
 
 menuButton.addEventListener("click", function () {
-  console.log("Ha")
+  if (menu.classList.contains("main-nav--opened")) {
+    menu.classList.remove("main-nav--opened");
+    menu.classList.add("main-nav--closed")
+  } else {
+    menu.classList.remove("main-nav--closed");
+    menu.classList.add("main-nav--opened")
+  }
 });
+
+mainLink.addEventListener("click", function () {
+  mainLink.classList.add("main-nav__link--active")
+})
